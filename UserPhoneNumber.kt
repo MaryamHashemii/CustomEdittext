647dec301eb64aaf682eba6ee4f287d6ba113aa9
NiloofarH <maryam.hashemy66@gmail.com>
2020-12-26 13:21:54 +0330

    Add files via upload

---
+++ UserPhoneNumber.kt
@@ -0,0 +1,34 @@
+package ir.app7030.android.data.model.api.user
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+
+data class UserPhoneNumber (
+
+    @SerializedName("phoneNumber")
+    var phoneNumber: String = "",
+
+    @SerializedName("class")
+    var clasz: String = "mobile",
+
+    @SerializedName("provider")
+    var provider: String? = "",
+
+    @SerializedName("nickname")
+    var nickname: String = ""
+
+) :Serializable{
+    companion object {
+        const val TYPE_NUMBER = 10
+        const val TYPE_ADD = 12
+        const val TYPE_TEXT_RECENTLY = 15
+    }
+
+
+    @Transient
+    var itemType  = TYPE_NUMBER
+}
+
+
